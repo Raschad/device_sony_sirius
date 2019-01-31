f790ba1961de3c8b4ccede55a486a4acac0183e7
Raschad <Raschad@yandex.ru>
2019-01-31 17:04:53 +0300

    RR-fy

--- rr_sirius.mk
+++ rr_sirius.mk
@@ -1,5 +1,5 @@
 ## Inherit AICP common telephony parts
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit AICP product configuration
 
@@ -9,12 +9,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503
 BUILD_FINGERPRINT := Sony/D6503/D6503:6.0.1/23.5.A.1.291/2769308465:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 6.0.1 23.5.A.1.291 2769308465 release-keys"
 
-PRODUCT_NAME := aicp_sirius
+PRODUCT_NAME := rr_sirius
 PRODUCT_DEVICE := sirius
 
 # AICP Device Maintainer
-PRODUCT_BUILD_PROP_OVERRIDES += \
-        DEVICE_MAINTAINERS="Hafyzy"
+# PRODUCT_BUILD_PROP_OVERRIDES += \
+#        DEVICE_MAINTAINERS="Hafyzy"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
