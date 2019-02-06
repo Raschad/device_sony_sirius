fd7e58a7398d9e47263598fdd4a78a26f1bed5e3
Raschad <Raschad@yandex.ru>
2019-02-06 12:59:32 +0300

    Update aosp_sirius.mk

--- aosp_sirius.mk
+++ aosp_sirius.mk
@@ -1,5 +1,5 @@
 ## Inherit AICP common telephony parts
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit AICP product configuration
 
@@ -8,6 +8,12 @@ $(call inherit-product, device/sony/sirius/device.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503
 BUILD_FINGERPRINT := Sony/D6503/D6503:6.0.1/23.5.A.1.291/2769308465:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 6.0.1 23.5.A.1.291 2769308465 release-keys"
+
+TARGET_GAPPS_ARCH := arm
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
 
 PRODUCT_NAME := aosp_sirius
 PRODUCT_DEVICE := sirius
